9a64ba1e15403539e7bd60e65feacb1739cbe806
XIAOHUI-WENG2020 <73845398+XIAOHUI-WENG2020@users.noreply.github.com>
2022-05-03 01:40:53 +0100

    version 1 basic, window size

---
+++ main.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require('electron');
+function createWindow() {
+ const win = new BrowserWindow({
+ width: 700,
+ height: 600,
+ webPreferences: {
+ nodeIntegration: true
+ },
+
+ });
+ win.loadFile('index.html');
+}
+app.on('ready',createWindow);
