7383357420e653de5cb2f87686b4d3de725a2236
XIAOHUI-WENG2020 <73845398+XIAOHUI-WENG2020@users.noreply.github.com>
2022-05-04 18:51:44 +0100

    fixed the window size

--- caDraft/main.js
+++ caDraft/main.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 function createWindow() {
  const win = new BrowserWindow({
- width: 480,
+ width: 600,
  height: 500,
  webPreferences: {
  nodeIntegration: true
